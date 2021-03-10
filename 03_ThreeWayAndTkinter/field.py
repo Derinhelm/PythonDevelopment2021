c34a4ab0477a09e63f44944d42fd1b54697ac880
Lena <Derinhelm@yandex.ru>
2021-03-10 22:15:29 +0300

    Перемещение клетки и проверка поля
    - Если можем переместить выбранную клетку на соседнюю ячейку, перемещаем
    - Если все клетки стоят на своих местах, фиксируем победу

--- 03_ThreeWayAndTkinter/field.py
+++ 03_ThreeWayAndTkinter/field.py
@@ -1,8 +1,10 @@
 import random
 
+
 class Field:
     def __init__(self):
-        self.cells = {(i, j):0 for i in range(1, 5) for j in range(1, 5)}
+        self.cells = {(i, j): 0 for i in range(1, 5) for j in range(1, 5)}
+        self.winFlag = False
         self.generateNew()
 
     def get(self):
@@ -13,6 +15,22 @@ class Field:
         cellPermutations = [i for i in range(1, 17)]
         random.shuffle(cellPermutations)
         self.cells = dict(zip(self.cells.keys(), cellPermutations))
+        self.winFlag = False
+
+    def checkWin(self):
+        labels = [self.cells[(i, j)] for i in range(1, 5) for j in range(1, 5)]
+        self.winFlag = (labels == [i for i in range(1, 17)])
 
     def isWin(self):
-        return self.cells[(4, 4)] == 16
+        return self.winFlag
+
+    def move(self, coord):
+        '''Передвигаем соседнюю пустышку, если есть'''
+        x, y = coord
+        for (dx, dy) in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            neigh_coord = (x + dx, y + dy)
+            if neigh_coord in self.cells and self.cells[neigh_coord] == 16:
+                self.cells[coord], self.cells[neigh_coord] = \
+                    self.cells[neigh_coord], self.cells[coord]
+                self.checkWin()
+                break
