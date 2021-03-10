209c9c25442731e3e1b8d108c2f23e7b88c31ad9
Lena <Derinhelm@yandex.ru>
2021-03-10 22:16:52 +0300

    Интерфейс игры
    - Окошко с сообщением о победе
    - Возможность растягивать клетки

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,31 +1,33 @@
 import time
 import tkinter as tk
 from field import Field
-
+from tkinter import messagebox as mb
 
 class Application(tk.Tk):
     def __init__(self, field):
         self.field = field
         tk.Tk.__init__(self)
         self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
         self.buttonFrame = self.createButtonFrame()
         self.cellFrame = self.createButtonVidget()
         self.drawField()
 
     def createButtonFrame(self):
         F1 = tk.LabelFrame(self, relief = tk.GROOVE)
-        F1.columnconfigure(0, weight=1)
-        F1.grid(sticky='NEWS')
+        F1.grid()
         N = tk.Button(F1, text = "New", command=self.new)
-        N.grid(row=0, column=0, sticky='NEWS')
+        N.grid(row=0, column=0)
         E = tk.Button(F1, text = "Exit", command=self.quit)
-        E.grid(row=0, column=1, sticky='NEWS')
+        E.grid(row=0, column=1)
         return F1
 
     def createButtonVidget(self):
         F2 = tk.LabelFrame(self, relief = tk.GROOVE)
-        F2.columnconfigure(0, weight=1)
-        F2.rowconfigure(0, weight=1)
+        for i in range(1, 5):
+            F2.columnconfigure(i, weight=1)
+            F2.rowconfigure(i, weight=1)
         F2.grid(sticky='NEWS')
         return F2
 
@@ -38,24 +40,16 @@ class Application(tk.Tk):
             real_lab = str(lab)
             if lab == 16:
                 real_lab = ""
-            B = tk.Button(self.cellFrame, text=real_lab)
+            B = tk.Button(self.cellFrame, text=real_lab, command= lambda x=(r,c): self.moveCell(x))
             B.grid(row=r, column=c, sticky='NEWS')
         self.cellFrame.grid(sticky='NEWS')
 
-
-    def showtime(self):
-        self.time.set(time.strftime("%c"))
-
-    def createWidgets(self):
-        self.time = tk.StringVar()
-        self.timeButton = tk.Button(self, text='Time', command=self.showtime)
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.timeLabel = tk.Label(self, textvariable=self.time)
-        self.timeButton.grid()
-        self.quitButton.grid(row=0, column=1)
-        self.timeLabel.grid(columnspan=2)
-
-
+    def moveCell(self, c):
+        self.field.move(c)
+        self.drawField()
+        if self.field.isWin():
+            mb.showinfo("Поздравление", "Вы победили!")
+            self.new()
 
 f = Field()
 app = Application(f)
