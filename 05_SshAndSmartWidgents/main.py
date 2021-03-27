a835d49aa8fa5a48211545d85134e189497e79bc
Lena <Derinhelm@yandex.ru>
2021-03-27 23:42:51 +0300

    Изменение картинки по тексту и текста по картинке

--- 05_SshAndSmartWidgents/main.py
+++ 05_SshAndSmartWidgents/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import re
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -7,14 +8,16 @@ class Application(tk.Frame):
         self.grid()
         self.t = tk.Text(self)
         self.t.grid()
-        self.tb = tk.Button(self, text="Обновить картинку по тексту")
+        self.t.tag_config("wrong", background="red")
+        self.tb = tk.Button(self, text="Обновить картинку по тексту", command=self.change_picture, font = 'Helvetica')
         self.tb.grid(row=1, column=0)
+
 
         self.c = tk.Canvas(self, bg='white')
         self.c.bind('<ButtonPress-1>', lambda event: self.create_oval_beg(event))
         self.c.bind('<ButtonRelease-1>', lambda event: self.create_oval_end(event))
         self.c.grid(row=0, column=1)
-        self.cb = tk.Button(self, text="Обновить текст по картинке", command=self.change_text)
+        self.cb = tk.Button(self, text="Обновить текст по картинке", command=self.change_text, font = 'Helvetica')
         self.cb.grid(row=1, column=1)
 
 
@@ -32,13 +35,31 @@ class Application(tk.Frame):
         if self.exist_flag:
             self.c.move(self.item, event.x - self.mouse_press[0], event.y - self.mouse_press[1])
         else:
-            self.c.create_oval(self.mouse_press[0], self.mouse_press[1], event.x, event.y, width = 7, outline = 'green')
+            self.c.create_oval(self.mouse_press[0], self.mouse_press[1], event.x, event.y, width = 7, outline = 'green', fill = 'limegreen')
 
     def change_text(self):
         self.t.delete('1.0', tk.END)
         for item in self.c.find_all():
-            s = str(self.c.coords(item)) + str(self.c.itemcget(item, 'width')) + str(self.c.itemcget(item, 'outline'))
+            s = "oval: coords - " + str(", ".join(map(str, self.c.coords(item)))) + ", width - "+ str(self.c.itemcget(item, 'width')) + ", color - " + str(self.c.itemcget(item, 'outline')) + ", fill_color - "+ str(self.c.itemcget(item, 'fill')) + ".\n"
             self.t.insert(tk.END, s)
+
+    def change_picture(self):
+        s = self.t.get("1.0", tk.END).splitlines()
+        self.t.tag_delete("wrong")
+        self.c.delete("all")
+        for i in range(len(s)):
+            if s[i]:
+                pattern = re.findall("oval: coords - (.*), (.*), (.*), (.*), width - (.*), color - (.*), fill_color - (.*).", s[i])
+                if pattern != []:
+                    coords = pattern[0][:4]
+                    width = pattern[0][4]
+                    color = pattern[0][5]
+                    fill_color = pattern[0][6]
+                    self.c.create_oval(*coords, width=width, outline=color, fill=fill_color)
+                else:
+                    self.t.tag_add("wrong", f"{i + 1}.0", f"{i + 1}.end")
+        self.t.tag_config("wrong", background="red")
+        
 
 app = Application()
 app.mainloop()
